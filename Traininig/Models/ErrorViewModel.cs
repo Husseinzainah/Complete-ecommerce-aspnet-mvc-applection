8ca3627c0641ba950082bd2a78b10f053a917732
hussin <hussin@DESKTOP-9RUVCNT>
2023-02-05 14:08:22 +0300

    Add project files.

---
+++ Traininig/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Traininig.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
